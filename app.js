6c2ce715317909f3b469e92112b16f8d1e52cdc0
PogodaIgor <igor.pogoda2010@wp.pl>
2025-11-20 08:36:18 +0100

    a

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-function wyswietl(){
-    var imie = document.querySelector("#imie").value
-    var nazwisko = document.getElementById("nazwisko").value
 
-    document.querySelector("h1").innerHTML = parseInt(imie) + parseInt(nazwisko)
-    document.querySelector("h1").innerHTML = parseInt(imie) - parseInt(nazwisko)
+function dodac(){
+    var liczba1 = document.querySelector("#liczba1").value
+    var liczba2 = document.querySelector("#liczba2").value
+    document.querySelector("#h").innerHTML = parseInt(liczba1) + parseInt(liczba2)
+}
+function odjac(){
+    var liczba1 = document.querySelector("#liczba1").value
+    var liczba2 = document.querySelector("#liczba2").value
+
+    document.querySelector("#h").innerHTML = parseInt(liczba1) - parseInt(liczba2)
+}
+function mnozyc(){
+    var liczba1 = document.querySelector("#liczba1").value
+    var liczba2 = document.querySelector("#liczba2").value
+
+    document.querySelector("#h").innerHTML = parseInt(liczba1) * parseInt(liczba2)
+}
+function dzielic(){
+    var liczba1 = document.querySelector("#liczba1").value
+    var liczba2 = document.querySelector("#liczba2").value
+
+    document.querySelector("#h").innerHTML = liczba1 / liczba2
+
+    if(liczba2 == 0){
+        document.querySelector("#h").innerHTML = "Nie można dzielić przez 0"
+    }
 }
